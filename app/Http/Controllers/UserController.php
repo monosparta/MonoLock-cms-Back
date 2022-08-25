3f16d2954ebe63683973c0bdaf1a1b6a44ce1810
Jonathan <jo901025@gmail.com>
2022-08-25 10:29:08 +0800

    fix Validator check user name was unique in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
             $request->all(),
             [
                 'mail' => 'required|unique:users|email:rfc|max:80',
-                'name' => 'required|unique:users|max:40',
+                'name' => 'required|max:40',
                 'password' => 'required|confirmed',
             ],
         );
@@ -310,7 +310,7 @@ class UserController extends Controller
             [
                 'lockerNo' => 'required|exists:lockers',
                 'mail' => 'required|unique:users|email:rfc|max:80',
-                'name' => 'required|unique:users|max:40',
+                'name' => 'required|max:40',
                 'cardId' => 'required|unique:users|digits_between:0,20',
                 'phone' => 'required|unique:users|digits_between:0,20',
             ],
@@ -420,7 +420,7 @@ class UserController extends Controller
                     }),
                 ],
                 'mail' => ['required', 'email:rfc', 'max:80', Rule::unique('users')->ignore($id)],
-                'name' => ['required', 'max:40', Rule::unique('users')->ignore($id)],
+                'name' => ['required', 'max:40'],
                 'cardId' => ['required', 'numeric', 'digits_between:6,20', Rule::unique('users')->ignore($id)],
                 'phone' => ['required', 'numeric', 'digits_between:1,20', Rule::unique('users')->ignore($id)],
             ],
