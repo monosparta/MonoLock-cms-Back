09c63f5447dd66f14e48df3c9b6fbd56c1db34e7
Luca <luca@trunk-studio.com>
2022-10-28 16:37:27 +0800

    fix: :bug: allow localhost

--- config/cors.php
+++ config/cors.php
@@ -20,6 +20,7 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'http://localhost:3000',
         'http://127.0.0.1:3000',
         'http://10.2.10.111',
         'https://10.2.10.110',
