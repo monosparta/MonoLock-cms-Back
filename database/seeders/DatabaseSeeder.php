7b6cc156a9fa2b2ec27847fd5e040b401b201aec
Bin <k1234988@gmail.com>
2023-03-01 18:29:31 +0800

    uuid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+     
         $this->call([
             LockerSeeder::class,
             RootUserSeeder::class,
--- database/seeders/RootUserSeeder.php
+++ database/seeders/RootUserSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Seeders;
-
+use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
@@ -18,20 +18,23 @@ class RootUserSeeder extends Seeder
     public function run()
     {
         User::create([
+            "id"=>Uuid::uuid4(),
             'permission' => 0,
             'mail' => '000@example.com',
             'name' => 'root',
             'password' => Hash::make('root'),
         ]);
         User::create([
+            "id"=>Uuid::uuid4(),
             'permission' => 0,
-            'mail' => '002@example.com',
+            'mail' => '001@example.com',
             'name' => 'root2',
             'password' => Hash::make('root'),
         ]);
         User::create([
+            "id"=>Uuid::uuid4(),
             'permission' => 0,
-            'mail' => '003@example.com',
+            'mail' => '002@example.com',
             'name' => 'root3',
             'password' => Hash::make('root'),
         ]);
