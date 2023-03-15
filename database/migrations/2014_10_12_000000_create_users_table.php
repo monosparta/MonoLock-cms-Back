29d90fe3fdad4ca9681a5c7f154195c58d8fff5f
ej0331 <k0970133227@gmail.com>
2023-03-15 17:43:58 +0800

    修改id欄位從流水號改為uuid型態

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->increments('id');
             $table->integer('permission')->default(1);
             $table->string('name',40);
             $table->string('password',80);
