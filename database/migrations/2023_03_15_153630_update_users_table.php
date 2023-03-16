fa60678d1761d3836b5b9420b6dfbfcdf59e716b
ej0331 <k0970133227@gmail.com>
2023-03-16 23:46:43 +0800

    uuid添加unique

--- database/migrations/2023_03_15_153630_update_users_table.php
+++ database/migrations/2023_03_15_153630_update_users_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
     {
         // 新增uuid欄位
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid('uuid')->after('id');
+            $table->uuid('uuid')->after('id')->unique();
         });
 
         // 塞值
