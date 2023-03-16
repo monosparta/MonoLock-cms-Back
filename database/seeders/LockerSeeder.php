1cf2e9d0fa23eb776cb997c0aea634b690f394a6
ej0331 <k0970133227@gmail.com>
2023-03-16 10:48:54 +0800

    修正index

--- database/seeders/LockerSeeder.php
+++ database/seeders/LockerSeeder.php
@@ -24,10 +24,9 @@ class LockerSeeder extends Seeder
         foreach ($lockerList as $value) {
             $locker = ['lockerNo' => $value];
             if ($value != NULL) {
-                $index = random_int(0, count($users));
+                $index = random_int(0, count($users) - 1);
                 $locker['lockerEncoding'] = $faker->unique()->regexify('[0-9]{4}');
                 $locker['userId'] = $users[$index]['id'];
-                error_log($users[$index]['id']);
                 array_splice($users, $index, 1);
             } else {
                 $locker['lockerEncoding'] = NULL;
