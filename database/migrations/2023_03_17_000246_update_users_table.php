49c819be4e0e7298cd8af03084202c6617b1d113
ej0331 <k0970133227@gmail.com>
2023-03-17 00:05:31 +0800

    test

---
+++ database/migrations/2023_03_17_000246_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\User;
+use Ramsey\Uuid\Uuid;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->uuid('id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->dropColumn('uuid');
+        // });
+    }
+};
